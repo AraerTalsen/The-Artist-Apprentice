da271dd83270005a6d050dff8b0d7386a567e953
MJMurphy99 <43189660+MJMurphy99@users.noreply.github.com>
2021-03-30 14:35:56 -0400

    Ink now disappears after defeating enemy

--- Assets/Scripts/Dialogue/OverworldManager.cs
+++ Assets/Scripts/Dialogue/OverworldManager.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Tilemaps;
 public class OverworldManager : MonoBehaviour
 {
     public static OverworldManager instance;
+
+    public List<GameObject> inkPortals;
+    public List<GameObject> inkTiles;
 
     public Transform Player;
 
@@ -14,17 +17,25 @@ public class OverworldManager : MonoBehaviour
         {
             instance = this;
         }
+
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < inkPortals.Count; i++)
+        {
+            if (inkPortals[i].activeSelf == false)
+            {
+                inkTiles[i].SetActive(false);
+            }
+        }
     }
 }
