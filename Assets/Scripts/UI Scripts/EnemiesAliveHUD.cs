ad7cb38ae5f877466e47881547481a40d74b1b3f
MJMurphy99 <43189660+MJMurphy99@users.noreply.github.com>
2021-03-31 01:18:31 -0400

    Final push before playtest
    Added new NPC's with sean, fixed the hud issue and tested it for tmro

---
+++ Assets/Scripts/UI Scripts/EnemiesAliveHUD.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EnemiesAliveHUD : MonoBehaviour
+{
+    public TextMeshProUGUI t;
+    
+
+    public void Start()
+    {
+        t.text = "Enemies: " + ActiveOverworldEntity.entityCount[1];
+
+        if (ActiveOverworldEntity.entityCount[1] == 0)
+        {
+            SceneManager.LoadScene("EndDemoScene");
+        }
+    }
+}
