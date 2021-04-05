c37faa7a70b4cd6e75ab1fc670972b8c9540e451
caurso99 <48004221+caurso99@users.noreply.github.com>
2021-04-05 14:33:55 -0400

    GameAudio Initializer Added
    Fixed GameAudio Singleton. Now the Singleton Instance is initialized on game start so we should have no problems with references to the GameAudio Scriptable Object in builds.

---
+++ Assets/Resources/Audio/GameAudioInitializer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Initializes Singleton Objects within the game, Runs once at game start
+/// </summary>
+
+public class GameAudioInitializer : MonoBehaviour
+{
+    //SingletonSO in build
+    [Tooltip("GameAudio SO")]
+    public GameAudio gameAudio;
+
+
+    void Awake()
+    {
+        //Initializes
+        gameAudio = GameAudio.Instance;
+    }
+}
